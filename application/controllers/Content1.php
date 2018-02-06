19fc4ab04acb8b7ecc337938bc259196d261578d
Rezki Kartika Permana <35755668+kiteleee@users.noreply.github.com>
2018-02-06 14:09:37 +0700

    Update Content1.php

--- application/controllers/Content1.php
+++ application/controllers/Content1.php
@@ -23,43 +23,33 @@ class Content1 extends CI_Controller {
     $this->load->view('footer');
   }
   public function tambah_aksi(){
-    $data = array();
-    
-    if($this->input->post('submit')){ // Jika user menekan tombol Submit (Simpan) pada form
-      // lakukan upload file dengan memanggil function upload yang ada di GambarModel.php
-      $upload = $this->Content1Model->upload();
-      
-      if($upload['result'] == "success"){ // Jika proses upload sukses
-         // Panggil function save yang ada di GambarModel.php untuk menyimpan data ke database
-        $this->Content1Model->save($upload);
-        
-        redirect('index'); // Redirect kembali ke halaman awal / halaman view data
-      }else{ // Jika proses upload gagal
-        $data['message'] = $upload['error']; // Ambil pesan error uploadnya untuk dikirim ke file form dan ditampilkan
-      }
-    }
-    
-    $this->load->view('content1', $data);
-  }
-  public function hapus($id_content1){
-    $where = array('id_content1' => $id_content1);
-    $this->Content1Model->hapus_data($where,'content1');
+    $nama = $this->input->post('nama');
+    $tanggal = $this->input->post('tanggal');
+    $keterangan = $this->input->post('keterangan');
+ 
+    $data = array(
+      'nama' => $nama,
+      'tanggal' => $tanggal,
+      'keterangan' => $keterangan
+      );
+    $this->Content1Model->input_data($data,'content1');
     redirect('index');
   }
   public function edit($id_content1){
+  $this->load->view('header');
   $where = array('id_content1' => $id_content1);
   $data['content1'] = $this->Content1Model->edit_data($where,'content1')->result();
-  $this->load->view('header');
   $this->load->view('edit_content1',$data);
   $this->load->view('footer');
-  }
-  public function edit_aksi(){
+}
+  public function update(){
   $id_content1 = $this->input->post('id_content1');
   $nama = $this->input->post('nama');
   $tanggal = $this->input->post('tanggal');
   $keterangan = $this->input->post('keterangan');
  
   $data = array(
+    'id_content1' => $id_content1,
     'nama' => $nama,
     'tanggal' => $tanggal,
     'keterangan' => $keterangan
@@ -72,5 +62,10 @@ class Content1 extends CI_Controller {
   $this->Content1Model->update_data($where,$data,'content1');
   redirect('index');
 }
+  public function hapus($id_content1){
+    $where = array('id_content1' => $id_content1);
+    $this->Content1Model->hapus_data($where,'content1');
+    redirect('index');
+  }
 
 }
