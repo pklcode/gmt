4b79c131dce623c10548a7feb6464721dc159055
Rezki Kartika Permana <35755668+kiteleee@users.noreply.github.com>
2018-02-12 11:37:52 +0700

    Update Content1.php

--- application/controllers/Content1.php
+++ application/controllers/Content1.php
@@ -1,9 +1,12 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Content1 extends CI_Controller {
-  
+
   public function __construct(){
     parent::__construct();
-    
+    if(!$this->session->userdata('username'))
+    {
+      redirect('login');
+    }
     $this->load->model('Content1Model');
     $this->load->helper('url');
   }
@@ -26,13 +29,16 @@ class Content1 extends CI_Controller {
     $nama = $this->input->post('nama');
     $tanggal = $this->input->post('tanggal');
     $keterangan = $this->input->post('keterangan');
+    $id_category = $this->input->post('id_category');
     $data = array(
       'nama' => $nama,
       'tanggal' => $tanggal,
-      'keterangan' => $keterangan
+      'keterangan' => $keterangan,
+      'id_category' => $id_category,
+      'gambar'=>$_FILES['gambar']['name']
       );
         $this->Content1Model->input_data($data,'content1');
-    $config['upload_path']          = './gambar/';
+    $config['upload_path']          = './gambar';
     $config['allowed_types']        = 'gif|jpg|png';
     $config['max_size']             = 100;
     $config['max_width']            = 1024;
@@ -46,7 +52,8 @@ class Content1 extends CI_Controller {
     }else{
       $data = array('upload_data' => $this->upload->data());
     }
-    redirect('index');
+    redirect('content1');
+    
   }
   public function edit($id_content1){
   $this->load->view('header');
@@ -60,25 +67,27 @@ class Content1 extends CI_Controller {
   $nama = $this->input->post('nama');
   $tanggal = $this->input->post('tanggal');
   $keterangan = $this->input->post('keterangan');
+  $id_category = $this->input->post('id_category');
  
   $data = array(
     'id_content1' => $id_content1,
     'nama' => $nama,
     'tanggal' => $tanggal,
-    'keterangan' => $keterangan
+    'keterangan' => $keterangan,
+    'id_category' => $id_category,
+    'gambar'=>$_FILES['gambar']['name']
   );
  
   $where = array(
     'id_content1' => $id_content1
   );
  
-  $this->Content1Model->update_data($where,$data,'content1');
-  redirect('index');
+  redirect('content1');
 }
   public function hapus($id_content1){
     $where = array('id_content1' => $id_content1);
     $this->Content1Model->hapus_data($where,'content1');
-    redirect('index');
+    redirect('content1');
   }
 
 }
